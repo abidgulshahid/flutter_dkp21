f032caacb89e5b7b1de08ef3d699ddf28b0679d8
silencemind <silencemind55@gmail.com>
2021-05-23 20:53:49 +0500

    nadia_first_app

--- app/src/androidTest/java/com/example/flutter_basic/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/flutter_basic/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.flutter_basic;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
---
+++ nadia/android/app/src/main/kotlin/com/example/nadia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nadia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
