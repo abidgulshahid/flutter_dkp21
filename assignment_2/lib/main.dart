8d56fc6d0edd180bf44e5d204debdd6fab057c34
silencemind <silencemind55@gmail.com>
2021-06-09 01:47:31 +0500

    Update main.dart

--- assignment_2/lib/main.dart
+++ assignment_2/lib/main.dart
@@ -4,3 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
+class Login_page extends StatelessWidget {
+  const Login_page({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    
+      
+    );
+  }
+}
